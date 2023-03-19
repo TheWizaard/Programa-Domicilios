44f5616c39f58057d6603bf346ea9a0e37c26d49
TheAlexander02 <Jhonatanescobar65@gmail.com>
2023-03-18 19:37:46 -0500

    owo

--- Programa de Domicilios/programa.java
+++ Programa de Domicilios/programa.java
@@ -4,8 +4,8 @@ import javax.swing.*;
 class program{  
     public static void main(String[] args) {
         
-        String Nombre_Usuario,Direccion,Correo_Usuario,Decision_Tienda;
-        int DocumentoID;
+        String Nombre_Usuario,Direccion,Correo_Usuario,;
+        int DocumentoID,Decision_Tienda,Eleccion_Helado;
 
         JOptionPane.showMessageDialog(null, "Bienvenido a Nuestra APP de Domicilios", "BIENVENIDO",JOptionPane.INFORMATION_MESSAGE);
         
@@ -17,6 +17,19 @@ class program{
 
         JOptionPane.showMessageDialog(null,"Sus datos han sido registrados correctamente","AFIRMACION",JOptionPane.INFORMATION_MESSAGE);
 
-        Decision_Tienda = JOptionPane.showInputDialog("SELECCIONE SU TIENDA\n 1)Heladeria Crepes and Waffles\n 2)Dominos Pizza\n 3)OXXO", "");
+        Decision_Tienda = Integer.parseInt(JOptionPane.showInputDialog("SELECCIONE SU TIENDA\n 1)Heladeria Crepes and Waffles\n 2)Dominos Pizza\n 3)OXXO",""));
+
+        switch (Decision_Tienda) {
+
+            case 1 -> {
+                JOptionPane.showMessageDialog(null,"Su eleccion se ha ejecutado correctamente\n BIENVENIDO a nuestra Heladeria Crepes and Waffles", "Creppes And Waffles",JOptionPane.INFORMATION_MESSAGE);
+
+                Eleccion_Helado = Integer.parseInt(JOptionPane.showInputDialog("NUESTRA TIENDA OFRECE LOS SIGUIENTES PRODUCTOS, SELECCIONE CUAL LE GUSTARIA ELEGIR", ""));
+
+
+            }
+        
+
+        }
     }        
 }
