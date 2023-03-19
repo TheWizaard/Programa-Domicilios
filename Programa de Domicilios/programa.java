5d55d5e66faed0a48441f145755f907519fd7a1c
TheAlexander02 <Jhonatanescobar65@gmail.com>
2023-03-18 19:05:26 -0500

    UWU

--- Programa de Domicilios/programa.java
+++ Programa de Domicilios/programa.java
@@ -1,10 +1,17 @@
 import javax.swing.*;
 
-
 class program{  
     public static void main(String[] args) {
         
+        string Nombre_Usuario,Direccion;
+        int DocumentoID;
+
+        JOption.Pane.showMessageDialog(null, "Bienvenido a Nuestra APP de Domicilios", "BIENVENIDO",JOptionPane.INFORMATION_MESSAGE);
+
         
+
         
+
+
     }        
 }
