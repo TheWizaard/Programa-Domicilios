df1abc6a258cd17c809a9b7566f676dbed5199dd
TheAlexander02 <Jhonatanescobar65@gmail.com>
2023-03-18 19:12:41 -0500

    pRIMERO

--- Programa de Domicilios/programa.java
+++ Programa de Domicilios/programa.java
@@ -1,13 +1,18 @@
 import javax.swing.*;
 
+
 class program{  
     public static void main(String[] args) {
         
-        string Nombre_Usuario,Direccion;
+        String Nombre_Usuario,Direccion,Correo_Usuario;
         int DocumentoID;
 
-        JOption.Pane.showMessageDialog(null, "Bienvenido a Nuestra APP de Domicilios", "BIENVENIDO",JOptionPane.INFORMATION_MESSAGE);
-
+        JOptionPane.showMessageDialog(null, "Bienvenido a Nuestra APP de Domicilios", "BIENVENIDO",JOptionPane.INFORMATION_MESSAGE);
+        
+        JOptionPane.showMessageDialog(null, "Digite los siguientes datos poara registrarlo en el sistema", "INGRESO DE DATOS",JOptionPane.INFORMATION_MESSAGE);
+        Nombre_Usuario = JOptionPane.showInputDialog("INTRODUZCA SU NOMBRE", "");
+        Direccion = JOptionPane.showInputDialog("INGRESE SU DIRECCION", "");
+        Correo_Usuario = JOptionPane.showInputDialog("INGRESE SU CORREO", "");
         
 
         
